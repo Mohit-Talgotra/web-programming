68205ec76bd4c6a4bea42d02b2396b131e7b6b42
Mohit-Talgotra <talgotram@gmail.com>
2025-01-19 16:35:38 +0530

    update: script.js updated

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const repoOwner = "Mohit-Talgotra";
-const repoName = "web_programming";
-const branch = "main";
+document.addEventListener("DOMContentLoaded", function () {
+    const repoOwner = "Mohit-Talgotra";
+    const repoName = "web-programming";
+    const branch = "main";
 
-fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents?ref=${branch}`)
-    .then(response => response.json())
-    .then(data => {
-        const fileList = document.getElementById("fileList");
-        data.forEach(item => {
-            if (item.type === "file" && item.name.endsWith(".html")) {
-                const li = document.createElement("li");
-                const link = document.createElement("a");
-                link.href = item.name;
-                link.textContent = item.name;
-                li.appendChild(link);
-                fileList.appendChild(li);
-            }
-        });
-    })
-    .catch(error => console.error('Error fetching repository contents:', error));
+    fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents?ref=${branch}`)
+        .then(response => response.json())
+        .then(data => {
+            const fileList = document.getElementById("fileList");
+            data.forEach(item => {
+                if (item.type === "file" && item.name.endsWith(".html")) {
+                    const li = document.createElement("li");
+                    const link = document.createElement("a");
+                    link.href = `/${repoName}/${item.name}`;
+                    link.textContent = item.name;
+                    li.appendChild(link);
+                    fileList.appendChild(li);
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching repository contents:', error));
+});
